636d4fb6e2d540f114287d2bc0433ea62f06bd71
Quinten Boogaard <paaq@hotmail.nl>
2022-06-28 02:32:59 +0200

    Fix dark mode shenanigans

--- petlistpro/src/App.tsx
+++ petlistpro/src/App.tsx
@@ -9,15 +9,22 @@ function App() {
   
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
-
+  const [status, setStatus] = useLocalStorage('status', defaultDark ? '750s' : '0s');
+  
   const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
+  }
+  const switchAnimation = () => {
+    const newStatus = status === '750s' ? '0s' : '750s';
+    setStatus(newStatus)
+    document.documentElement.style.setProperty('--speed', newStatus);
   }
   return (
     <div className="background" data-theme={theme}>
     <div className="petlistPro" data-theme={theme}>
       <button className='modeSwitch' onClick={switchTheme}>{theme === 'light' ? '🌚' : '☀️'}</button>
+      <button id='animationToggle' className='modeSwitch' onClick={switchAnimation}>{status === '0s' ? '⏯️' : '⏸️'}</button>
       <div><Petlist/></div>
     </div>
     <div className='footer'>
